4b7d155a5edc860589f71da892d7748fca39f05f
Greg Chambers <grg.sox@gmail.com>
2012-03-26 10:39:27 -0400

    Completed stubbing out Philosopher class.

--- src/Philosopher.java
+++ src/Philosopher.java
@@ -4,5 +4,30 @@
  * @author Yin Poon
  */
 public class Philosopher extends Thread {
-
-}
+	
+	/**
+	 * Constructor for the Philosopher threaded class.
+	 * 
+	 * @param int id
+	 * @param Fork left
+	 * @param Fork right
+	 * @param boolean rHanded
+	 * @param int nTimes
+	 * @param long thinkMillis
+	 * @param long eatMillis
+	 */
+	public Philosopher(int id, Fork left, Fork right, boolean rHanded,
+			int nTimes, long thinkMillis, long eatMillis){
+		//TODO
+		
+	} // end constructor Philosopher
+	
+	/**
+	 * Override for the run method in the Thread class.
+	 */
+	public void run(){
+		//TODO
+		
+	} // end method run
+	
+} // end class Philosopher
